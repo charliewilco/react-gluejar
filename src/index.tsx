49c81a52f91515aed5b26cee4e534326a9f894f9
Charles ⚡ <charlespeters42@gmail.com>
2019-10-04 22:27:29 -0700

    Adding workflow

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from "react"
 
 /* <PasteContainer
   onPaste={files => files.length > 0 && this.props.onFileAccepted(files[0], files[0])}
@@ -30,12 +30,12 @@ export interface GluejarProps {
 type BlobLikeFile = File | null
 
 export class Gluejar extends React.Component<GluejarProps, GluejarState> {
-  static displayName = 'Gluejar'
+  static displayName = "Gluejar"
 
   static defaultProps = {
     onPaste: () => null,
     errorHandler: () => null,
-    acceptedFiles: ['image/gif', 'image/png', 'image/jpeg', 'image/bmp']
+    acceptedFiles: ["image/gif", "image/png", "image/jpeg", "image/bmp"]
   }
 
   state = {
@@ -78,7 +78,7 @@ export class Gluejar extends React.Component<GluejarProps, GluejarState> {
 
   componentDidMount() {
     const elm: Element = this.getContainer()
-    elm.addEventListener('paste', this.pasteHandler)
+    elm.addEventListener("paste", this.pasteHandler)
   }
 
   componentDidUpdate() {
@@ -87,7 +87,7 @@ export class Gluejar extends React.Component<GluejarProps, GluejarState> {
 
   componentWillUnmount() {
     const elm: HTMLElement = this.getContainer()
-    elm.removeEventListener('paste', this.pasteHandler)
+    elm.removeEventListener("paste", this.pasteHandler)
   }
 
   render() {
@@ -95,4 +95,96 @@ export class Gluejar extends React.Component<GluejarProps, GluejarState> {
     const { children } = this.props
     return children ? children({ images }) : null
   }
+}
+
+export interface GlueJarOptions {
+  acceptedFiles: string[]
+}
+
+interface IGlueJarReducerState {
+  pasted: string[]
+  error?: string
+}
+
+export enum GlueActions {
+  SET_FILE = "SET_FILES",
+  SET_ERROR = "SET_ERROR"
+}
+
+interface IAction {
+  type: GlueActions
+  payload: string
+}
+
+function reducer(state: IGlueJarReducerState, action: IAction): IGlueJarReducerState {
+  switch (action.type) {
+    case GlueActions.SET_FILE:
+      return { ...state, pasted: [action.payload, ...state.pasted], error: "" }
+    case GlueActions.SET_ERROR:
+      return { ...state, error: action.payload }
+    default:
+      throw new Error("Must specify action type")
+  }
+}
+
+export default function useGlueJar(
+  ref: React.MutableRefObject<HTMLElement>,
+  options: Partial<GlueJarOptions> = {
+    acceptedFiles: ["image/gif", "image/png", "image/jpeg", "image/bmp"]
+  }
+) {
+  const [state, dispatch] = React.useReducer<React.Reducer<IGlueJarReducerState, IAction>>(
+    reducer,
+    {
+      pasted: [],
+      error: ""
+    }
+  )
+
+  const isValidFormat = (fileType: string): boolean =>
+    options.acceptedFiles!.includes(fileType)
+  const transformImages = (data: DataTransfer): void => {
+    // NOTE: This needs to be a for loop, it's a list like object
+    for (let i = 0; i < data.items.length; i++) {
+      if (isValidFormat(data.items[i].type) !== false) {
+        let blob: BlobLikeFile = data.items[i].getAsFile()
+        let URL = window.URL
+
+        if (blob) {
+          // We shouldn't fire the callback if we can't create `new Blob()`
+          let src = URL.createObjectURL(blob)
+
+          dispatch({ type: GlueActions.SET_FILE, payload: src })
+        }
+      } else {
+        dispatch({
+          type: GlueActions.SET_ERROR,
+          payload: `Sorry, that's not a format we support`
+        })
+      }
+    }
+  }
+
+  const pasteHandler = (e: ClipboardEvent): void => {
+    e.clipboardData && e.clipboardData.items.length > 0
+      ? transformImages(e.clipboardData)
+      : dispatch({
+          type: GlueActions.SET_ERROR,
+          payload: `Sorry, to bother you but there was no image pasted.`
+        })
+  }
+
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.addEventListener("paste", pasteHandler)
+    }
+
+    return () => {
+      if (ref.current) {
+        ref.current.removeEventListener("paste", pasteHandler)
+      }
+    }
+  }, [ref.current, options])
+
+  return state
 }
