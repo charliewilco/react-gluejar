3db5e8acc1f6066d4db7f12381098844cf4fc518
Charlie ⚡ <4588060+charliewilco@users.noreply.github.com>
2021-07-16 03:42:10 -0700

    using vite

--- example/pastecontainer.tsx
+++ example/pastecontainer.tsx
@@ -1,55 +1,37 @@
-import * as React from 'react'
-import { render } from 'react-dom'
-import { Gluejar } from '../src'
-import { Header } from './header'
-
-const styles: React.CSSProperties = {
-  maxWidth: 600,
-  margin: `1rem auto`,
-  textAlign: 'center'
-}
+import React from "react";
+import { render } from "react-dom";
+import { useGlueJar } from "../src";
+import { Header, Instructions } from "./header";
 
 const imageStyle = {
   maxWidth: `100%`,
-  display: 'inline-block',
-  verticalAlign: 'middle',
-  fontStyle: 'italic',
-  marginBottom: 8
-}
+  display: "inline-block",
+  verticalAlign: "middle",
+  fontStyle: "italic",
+  marginBottom: 8,
+};
 
-const Instructions = () => (
-  <h2 style={{ fontWeight: 300, marginBottom: 24 }}>
-    Copy an image from your file system and paste it here
-  </h2>
-)
+const App = () => {
+  const { pasted, error } = useGlueJar();
 
-const Sorry = ({ error, onClose }) => (
-  <div>
-    <h3>
-      {error} <button onClick={onClose}>&times;</button>
-    </h3>
-  </div>
-)
-
-class App extends React.Component<any, any> {
-  onError = err => console.error(err)
-
-  render() {
-    return (
-      <div style={styles}>
-        <Header />
-        <Instructions />
-        <Gluejar onPaste={files => console.log(files)} onError={this.onError}>
-          {({ images }) =>
-            images.length > 0 &&
-            images.map(image => (
-              <img src={image} key={image} alt={`Pasted: ${image}`} style={imageStyle} />
-            ))
-          }
-        </Gluejar>
+  return (
+    <div
+      style={{
+        maxWidth: 600,
+        margin: `1rem auto`,
+        textAlign: "center",
+      }}>
+      <Header />
+      <Instructions />
+      {error !== null && <span>{error}</span>}
+      <div>
+        {pasted.length > 0 &&
+          pasted.map((image) => (
+            <img src={image} key={image} alt={`Pasted: ${image}`} style={imageStyle} />
+          ))}
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById("root"));
